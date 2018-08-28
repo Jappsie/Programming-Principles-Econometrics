79bc0837c6bc4a1f5d6f74629744ef4819b0b3fe
jaspe <jaspe@145.108.76.27>
2018-08-28 14:34:54 +0200

    Added sX and printed it

---
+++ Programming Principles/src/vars.py
@@ -0,0 +1,35 @@
+'''
+vars.py
+
+Purpose:
+    ...
+
+Version:
+    0            Assign/print String
+    
+Date:
+    28 Aug 2018
+    
+@author: jbn601
+'''
+########################################################
+### Imports
+# import numpy as np
+# import scipy as sp
+# import matplotlib.pyplot as plt
+# from gurobi import *
+
+#######################################################
+### Main
+def main():
+    # Magic numbers
+    sX = "String"
+    # Initialisation
+    
+    # Output
+    print("The String I made was \"%s\"" % (sX))
+####################################################
+### start main
+if __name__ == '__main__':
+    main()
+    
