8332508fec9710f69644d0609ed892b281951b4f
jaspe <jaspe@145.108.76.27>
2018-08-28 17:20:44 +0200

    Added:

--- Programming Principles/src/vars.py
+++ Programming Principles/src/vars.py
@@ -24,10 +24,17 @@ Date:
 def main():
     # Magic numbers
     sA = "String"
+    iB = 1
+    dC = 1.
+    bD = ((9 > 11) and (3 < 1)) or (type(iB) == type(dC))
     # Initialisation
     
     # Output
     print("The String I choose is \"%s\".\n" % (sA))
+    print("The Integer I choose is \"%i\".\n" % (iB))
+    print("The Double I choose is \"%.2g\".\n" % (dC))
+    print("The Boolean I choose is very complicated and equal to \"%r\".\n" % (bD))
+    
 ####################################################
 ### start main
 if __name__ == '__main__':
