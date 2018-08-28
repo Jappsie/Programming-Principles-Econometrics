15ead196f85edae349b8313189e71d43f81da9af
jaspe <jaspe@145.108.76.27>
2018-08-28 18:23:49 +0200

    Added multiple functions, can return more than 1 value!

--- Programming Principles/src/function.py
+++ Programming Principles/src/function.py
@@ -1,7 +1,7 @@
 '''
 C:/Users/jaspe/Documents/School/VU/Python Programming/VU/TestProject/src/function.py
 Purpose:
-    ...
+    Fun with functions
 
 Version:
     1            First Starts
@@ -52,16 +52,40 @@ def passValue():
     
     # Output
     return dRes
+
+#######################################################
+### passValues
+def passValues():
+    '''
+    Purpose:
+        Assign two predetermined (random) values
+    Inputs:
+        
+    Return value:
+        dRes1        double, assigned random value
+        dRes2        double, assigned random value
+    '''
+    # Magic numbers
+    dRes1 = np.random.normal(0,1,1)
+    dRes2 = np.random.exponential(10,1)
+    # Initialisation
+    
+    # Output
+    return dRes1, dRes2
 #######################################################
 ### Main
 def main():
     # Magic numbers
     sA = "Getal"
     # Initialisation
+    np.random.seed(9999)
     dB = passValue()
+    dC,dD = passValues()
     # Output
     printStr(sA)
     print(dB)
+    print(dC)
+    print(dD)
     
 ####################################################
 ### start main
